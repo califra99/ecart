104311b5a9323b6487181f84db68a3d7d79375e3
fcalian <matteo.brazzarola@ferroli.com>
2023-12-26 14:02:49 +0100

    created models and factory for coupons, products, nations and users

---
+++ database/factories/CouponFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Coupon>
+ */
+class CouponFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'code' => fake()->unique()->word(),
+            'value' => 10,
+            'active' => 1,
+            'min_price' => 0,
+            'max_price' => 1000
+        ];
+    }
+}
